e53c72e2ea2ce9ec596ac587f7305540a44b777b
Aleksei Shastkevich <ashastkevich@gmail.com>
2026-06-04 16:13:07 +0300

    Seperated DB

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'omegtzfmfijsxwbegugd.supabase.co',
         pathname: '/storage/v1/object/public/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'ufsnampjvboeyvymukey.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 }
